c12587532b0a31d9e18efafec09425f6086c8dad
code-with-zen <rajanlakhani@gmail.com>
2021-11-22 16:44:01 -0800

    ready for prod

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ createWebsiteButton.addEventListener("click", async () => {
   // get API token
   const apiTokenInput = document.getElementById("api-token-id").value;
 
-  // get website title input
-  const websiteTitleInput = await sanitizeInput(
-    document.getElementById("website-title-id").value
-  );
-
   // select file input
   const fileInput = document.getElementById("image-file-id");
 
+  const headerTitleInput = document.getElementById("header-title-id");
+
+  // select alt text for Image
+  const altImage = document.getElementById("image-alt-text-id").value;
+
   // enable button after required fields are filled in
-  if (apiTokenInput != "" && websiteTitleInput != "" && fileInput.value != "") {
-    uploadFile(fileInput.files[0], apiTokenInput, websiteTitleInput);
+  if (
+    apiTokenInput != "" &&
+    headerTitleInput.value != "" &&
+    fileInput.value != ""
+  ) {
+    uploadFile(fileInput.files[0], apiTokenInput, altImage, headerTitleInput);
   } else {
     document.getElementById("buttonRequired-id").style.color = "orange";
     document.getElementById("buttonRequired-id").innerText =
@@ -26,22 +30,31 @@ createWebsiteButton.addEventListener("click", async () => {
   }
 });
 
-async function uploadFile(file, apiToken, websiteTitle) {
+async function uploadFile(file, apiToken, altImage, headerTitleInput) {
   // create random 4 digit number to uniquely identify folder name
   var val = Math.floor(1000 + Math.random() * 9000);
   console.log(String(val));
+
+  const websiteDiv = document.getElementById("website-div-id");
+  const websiteLink = document.getElementById("web3-website-link-id");
+  const fileInput = document.getElementById("image-file-id");
+  // get website title input
+
   // add file to FormData object
   const userFormData = new FormData();
   userFormData.append("folderName", String(val));
   userFormData.append("image", file);
-  userFormData.append("websiteTitle", websiteTitle);
+  userFormData.append("headerTitle", headerTitleInput.value);
   userFormData.append("token", apiToken);
+  userFormData.append("altImage", altImage);
   console.log([...userFormData]);
   createWebsiteButton.classList.add("loading");
   createWebsiteButton.innerText = "wait";
+  websiteDiv.style.visibility = "hidden";
+  document.getElementById("buttonRequired-id").innerText = "";
 
   // send `POST` request
-  await fetch("http://localhost:8080/upload-files", {
+  await fetch("https://web3-website-maker.herokuapp.com/upload-files", {
     mode: "cors",
     method: "POST",
     headers: {
@@ -54,7 +67,8 @@ async function uploadFile(file, apiToken, websiteTitle) {
       if (res.status == 403) {
         console.log("API token is incorrect");
         document.getElementById("api-token-id").style.borderColor = "red";
-        document.getElementById("incorrectAPI-id").innerText = "API token is incorrect";
+        document.getElementById("incorrectAPI-id").innerText =
+          "API token is incorrect";
         createWebsiteButton.classList.remove("loading");
         createWebsiteButton.innerText = "Create Website on Web3.storage";
         throw Error("res status error: " + res.status);
@@ -67,45 +81,10 @@ async function uploadFile(file, apiToken, websiteTitle) {
     })
     .then((data) => {
       //    console.log(data.url);
-      const websiteDiv = document.getElementById("website-div-id");
-      const websiteLink = document.getElementById("web3-website-link-id");
-      const fileInput = document.getElementById("image-file-id");
-      // get website title input
-      const websiteTitleInput = document.getElementById("website-title-id");
       websiteDiv.style.visibility = "visible";
       websiteLink.href = data.url;
-      websiteTitleInput.value = "";
+      headerTitleInput.value = "";
       fileInput.value = "";
     })
     .catch((err) => console.error(err));
-}
-
-// sanitize input
-var tagBody = "(?:[^\"'>]|\"[^\"]*\"|'[^']*')*";
-
-var tagOrComment = new RegExp(
-  "<(?:" +
-    // Comment body.
-    "!--(?:(?:-*[^->])*--+|-?)" +
-    // Special "raw text" elements whose content should be elided.
-    "|script\\b" +
-    tagBody +
-    ">[\\s\\S]*?</script\\s*" +
-    "|style\\b" +
-    tagBody +
-    ">[\\s\\S]*?</style\\s*" +
-    // Regular name
-    "|/?[a-z]" +
-    tagBody +
-    ")>",
-  "gi"
-);
-
-async function sanitizeInput(html) {
-  var oldHtml;
-  do {
-    oldHtml = html;
-    html = html.replace(tagOrComment, "");
-  } while (html !== oldHtml);
-  return html.replace(/</g, "&lt;");
 }
